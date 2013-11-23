5bea9550fed930b1727c2ddc189f70d1a651e183
sarfaraz hussain <Sarfarazsajjad@gmail.com>
2013-11-23 23:34:44 +0500

    Initial Commit

---
+++ Parents PopUp/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Parents PopUp
+//
+//  Created by sarfaraz hussain on 23/11/2013.
+//  Copyright sarfaraz hussain 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Parents PopUp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Parents PopUp
+//
+//  Created by sarfaraz hussain on 23/11/2013.
+//  Copyright sarfaraz hussain 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
